aad7c18efdab7a95607fee0486fa707ea70a5bec
Rafał Kazik <kazik.raf@gmail.com>
2022-02-28 19:31:37 +0100

    correctly adds 2 numbers

--- src/rpn/rpn.test.ts
+++ src/rpn/rpn.test.ts
@@ -1,19 +1,11 @@
-import { rpn } from './rpn';
+import rpn from './rpn';
 
 describe('rpn()', () => {
-  it('returs number when passed', () => {
-    expect(rpn('2')).toBe(2);
-  });
+    it('returs number when passed', () => {
+        expect(rpn('2')).toBe(2);
+    });
 
-  it('correctly adds 2 numbers', function () {
-    expect(rpn('2 0 +')).toBe(2);
-  });
-
-  it(`correctly adds 2 numbers`, async () => {
-    expect(rpn('2 2 +')).toBe(4);
-  });
-
-  it('correctly divides  2  numbers', () => {
-    expect(rpn('2 2 -')).toBe(0);
-  });
+    it('correctly adds 2 numbers', () => {
+        expect(rpn('1 3 +')).toBe(4);
+    });
 });
